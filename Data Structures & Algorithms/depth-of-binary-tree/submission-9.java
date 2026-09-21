999c41aadc4f86fb2e7b12b7b99d01c22ec44201
Ayoub Ameur <ayoubameur.tech@gmail.com>
2026-09-21 12:57:28 +0100

    Add: depth-of-binary-tree - submission-9

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-9.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+            
+
+    public int maxDepth(TreeNode root) {
+
+        
+
+        if(root == null) return 0;
+
+
+        
+
+        int rightDepth = maxDepth(root.right);
+        int leftDepth = maxDepth(root.left);
+
+        if(rightDepth > leftDepth){
+            return 1+rightDepth;
+        }else{
+
+            return 1+leftDepth;
+        }
+
+
+
+
+        
+        
+        
+        }
+}
